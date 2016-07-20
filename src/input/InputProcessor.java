bd825c5278143d0bb7588904e5213f935f293a8e
rsickora <r.sickora@gmail.com>
2016-07-20 12:49:31 -0500

    refactored input processor to externalize regex validations to their own methods and be more descriptive of what the expressions are doing

--- src/input/InputProcessor.java
+++ src/input/InputProcessor.java
@@ -9,16 +9,23 @@ public class InputProcessor {
 
 	public void process(String testInput, PrintWriter outputWriter) {
 		
-		if(testInput.matches("^-?\\d+$")){
+		if(isValidIntegerWithinString(testInput)){
 			outputWriter.print(new ArabicToNumeralAdapter().adapt(Integer.parseInt(testInput))+"\n");
 		}
-		else if(testInput.isEmpty() == false && testInput.matches("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$")){
+		else if(isValidRomanNumeralString(testInput)){
 			outputWriter.print(new NumeralToArabicAdapter().adapt(testInput)+"\n");
 		}
 		
 		outputWriter.print("Application Terminated");
 
 	}
-
+	
+	private boolean isValidIntegerWithinString(String stringToValidate){
+		return stringToValidate.matches("^-?\\d+$");
+	}
+	
+	private boolean isValidRomanNumeralString(String stringToValidate){
+		return (stringToValidate.isEmpty() == false && stringToValidate.matches("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"));
+	}
 
 }
